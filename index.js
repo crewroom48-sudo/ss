d5e8c550574b5ebc4a70c21619693ad6f8ff64c2
crewroom48-sudo <crewroom48@gmail.com>
2026-05-25 22:37:30 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,38 @@
+{
+  "expo": {
+    "name": "shift-v2-",
+    "slug": "snack-49ade257-d493-400a-a858-9040ad8885b0",
+    "version": "1.0.0",
+    "orientation": "portrait",
+    "icon": "./assets/icon.png",
+    "userInterfaceStyle": "light",
+    "newArchEnabled": true,
+    "splash": {
+      "image": "./assets/splash-icon.png",
+      "resizeMode": "contain",
+      "backgroundColor": "#ffffff"
+    },
+    "ios": {
+      "supportsTablet": true
+    },
+    "android": {
+      "adaptiveIcon": {
+        "foregroundImage": "./assets/adaptive-icon.png",
+        "backgroundColor": "#ffffff"
+      },
+      "edgeToEdgeEnabled": true
+    },
+    "web": {
+      "favicon": "./assets/favicon.png"
+    },
+    "plugins": [
+      [
+        "expo-notifications",
+        {
+          "icon": "./assets/icon.png",
+          "color": "#ffffff"
+        }
+      ]
+    ]
+  }
+}
